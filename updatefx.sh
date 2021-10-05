ab66f9f817454f52487af8d40bfe7f7523cd474b
Zhaslan10 <zhas_86@mail.ru>
2021-10-05 12:03:28 +0600

    second commit

---
+++ updatefx.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+#Обновление Firefox
+ /usr/bin/sudo tar -jxvf /home/techman/EUBANK/firefox-92.0.tar.bz2 -C /opt/
+ /usr/bin/sudo rm /usr/bin/firefox
+ /usr/bin/sudo rm /usr/local/bin/firefox
+
+ /usr/bin/sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+ /usr/bin/sudo ln -s /opt/firefox/firefox /usr/bin/firefox
